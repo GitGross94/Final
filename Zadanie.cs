77d431c3bdb53d867dfc72c70c85fa06d995cfad
Boris <twinlab.forever@yandex.ru>
2023-06-13 01:36:10 +0500

    Добавление решения

--- Zadanie.cs
+++ Zadanie.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿﻿// Задача: Написать программу, которая из имеющегося массива строк 
+// формирует массив из строк, длина которых меньше, либо равна 3 символа. 
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте 
+// выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
+// лучше обойтись исключительно массивами.
+
+// Примеры: [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”] 
+// [“1234”, “1567”, “-2”, “computer science”] → [“-2”] 
+// [“Russia”, “Denmark”, “Kazan”] → []
+
+Console.WriteLine("Введите количество элементов массива: ");
+int size  =  Convert.ToInt32(Console.ReadLine());
+
+string[] array1 = new string[size];
+
+for (int i = 0; i < size; i++)
+{
+	Console.Write("Введите элемент массива: ");
+    string result = Console.ReadLine();
+	array1[i] = result;  
+}
+Console.WriteLine();
+Console.Write("Введенный массив: [");
+Console.Write(string.Join(",", array1));
+Console.Write("]");
+
+
+int count = 0;
+int max = 3;
+
+for (int i = 0; i < array1.Length; i++)
+{
+	if(array1[i].Length <= max) count++;
+}
+Console.WriteLine();
+Console.Write("Полученный массив: [");
+
+
+string[] array2 = new string[count];
+int j = 0;
+for (int i = 0; i < array1.Length; i++)
+{
+    if(array1[i].Length <= max)
+    {
+        array2[j] = array1[i];
+        Console.Write(array2[j] + ",");
+        j++;
+    }
+}
+Console.Write("]");
